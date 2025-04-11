f1e49ce410abb87ae80d2008a5927ab1ada19aa9
David Adamson <david.adamson2304@gmail.com>
2025-04-11 17:44:02 +0200

    refactor: move manifest json completely into vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
 import { defineConfig } from "vite";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import webExtension from "vite-plugin-web-extension";
 import pkg from "./package.json";
 
 export default defineConfig({
   plugins: [
     webExtension({
       manifest: () => ({
-        ...readJsonFile("manifest.json"),
         name: pkg.name,
         description: pkg.description,
         version: pkg.version,
+        manifest_version: 3,
+        icons: {
+          16: "icon/16.png",
+          32: "icon/32.png",
+          48: "icon/48.png",
+          96: "icon/96.png",
+          128: "icon/128.png",
+        },
+        content_scripts: [
+          {
+            matches: ["*://*/*"],
+            js: ["src/main.js"],
+            world: "MAIN",
+          },
+        ],
       }),
     }),
   ],
