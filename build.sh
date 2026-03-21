abc84bb9c01676414855376c0e531444942fd1c7
David Adamson <david.adamson2304@gmail.com>
2026-03-21 23:30:27 +0100

    logging

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cp LICENSE dist/LICENSE
 
 IFS=$'\n'
 for dep_path in $(npm ls --depth=0 --omit=dev --parseable --long=false 2>/dev/null | tail -n +2); do
+  echo $dep_path
   if [ -f "$dep_path/LICENSE" ]; then
     dep_name=$(basename "$dep_path")
     echo "$dep_name@$(npm info "$dep_name" version 2>/dev/null):" >> dist/THIRD_PARTY_LICENSES
