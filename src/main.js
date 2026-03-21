c3a8fdd874de5255c10f9ea3c46a786d17d59621
David Adamson <david.adamson2304@gmail.com>
2026-03-21 22:33:59 +0100

    feat: framework agnostic scanning

--- src/main.js
+++ src/main.js
@@ -1,42 +1,61 @@
 import { devtoolsPlugin, registerPiniaDevtools } from "pinia";
 
-setTimeout(() => {
-  const devtools = window.__VUE_DEVTOOLS_GLOBAL_HOOK__;
-  if (!devtools) return;
+const devtools = window.__VUE_DEVTOOLS_GLOBAL_HOOK__;
+if (!devtools) {
+  throw new Error(
+    "[single-spa-vue-force-dev] official vue devtools are not installed. Please find them here: https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd",
+  );
+}
+devtools.enabled = true;
 
-  function enablePiniaDevtools(app) {
-    const pinia = app.config.globalProperties.$pinia;
-    if (!pinia) return;
-    pinia.use(devtoolsPlugin);
-    registerPiniaDevtools(app, pinia);
+function enablePiniaDevtools(app) {
+  const pinia = app.config.globalProperties.$pinia;
+  if (!pinia) return;
+  pinia.use(devtoolsPlugin);
+  registerPiniaDevtools(app, pinia);
+}
+
+function initApp(app) {
+  if (devtools.apps.includes(app)) return;
+
+  devtools.emit("app:init", app, app.version, {
+    Fragment: Symbol.for("v-fgt"),
+    Text: Symbol.for("v-txt"),
+    Comment: Symbol.for("v-cmt"),
+    Static: Symbol.for("v-stc"),
+  });
+
+  app.onUnmount(() => {
+    devtools.emit("app:unmount", app);
+  });
+
+  try {
+    enablePiniaDevtools(app);
+  } catch (e) {
+    console.error("[single-spa-vue-force-dev] failed to enable pinia devtools.", e);
   }
+}
 
-  function initApp(app) {
-    if (devtools.apps.includes(app)) return;
+function startMonitoring() {
+  // init vue apps mounted after initial scan
+  Object.defineProperty(Element.prototype, "__vue_app__", {
+    set(app) {
+      Object.defineProperty(this, "__vue_app__", {
+        value: app,
+        writable: true,
+        configurable: true,
+        enumerable: true,
+      });
+      initApp(app);
+    },
+  });
 
-    devtools.emit("app:init", app, app.version, {
-      Fragment: Symbol.for("v-fgt"),
-      Text: Symbol.for("v-txt"),
-      Comment: Symbol.for("v-cmt"),
-      Static: Symbol.for("v-stc"),
-    });
-
-    try {
-      enablePiniaDevtools(app);
-    } catch (e) {
-      console.error("[single-spa-vue-force-dev] failed to enable pinia devtools.", e);
-    }
+  // initial scan
+  const nodes = document.querySelectorAll("*");
+  for (let i = 0; i < nodes.length; i++) {
+    const app = nodes[i]["__vue_app__"];
+    if (app) initApp(app);
   }
+}
 
-  function scan() {
-    const nodes = document.querySelectorAll("*");
-    for (let i = 0; i < nodes.length; i++) {
-      const app = nodes[i]["__vue_app__"];
-      if (app) initApp(app);
-    }
-  }
-
-  devtools.enabled = true;
-  setTimeout(scan, 2000);
-  window.addEventListener("single-spa:app-change", () => setTimeout(scan, 2000));
-}, 1000);
+startMonitoring();
