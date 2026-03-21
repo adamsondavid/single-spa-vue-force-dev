a2194c0d0c1163b588a80c99a0f12b35f38a7d33
David Adamson <david.adamson2304@gmail.com>
2026-03-21 23:36:04 +0100

    fix: license generation (#26)
    * logging
    
    * fix
    
    * fix: remove debugging

--- build.sh
+++ build.sh
@@ -4,15 +4,13 @@ rm -rf dist
 vite build
 cp LICENSE dist/LICENSE
 
-IFS=$'\n'
-for dep_path in $(npm ls --depth=0 --omit=dev --parseable --long=false 2>/dev/null | tail -n +2); do
+npm ls --depth=0 --omit=dev --parseable --long=false --silent 2>/dev/null | tail -n +2 | while IFS= read -r dep_path; do
   if [ -f "$dep_path/LICENSE" ]; then
     dep_name=$(basename "$dep_path")
     echo "$dep_name@$(npm info "$dep_name" version 2>/dev/null):" >> dist/THIRD_PARTY_LICENSES
     echo >> dist/THIRD_PARTY_LICENSES
 
-    license_path="node_modules/$dep_name/LICENSE"
-    cat "$license_path" >> dist/THIRD_PARTY_LICENSES
+    cat "node_modules/$dep_name/LICENSE" >> dist/THIRD_PARTY_LICENSES
 
     echo >> dist/THIRD_PARTY_LICENSES
     echo "---------------------------------------------------------" >> dist/THIRD_PARTY_LICENSES
